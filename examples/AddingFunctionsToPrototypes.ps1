6da6a8632c160681b23455ed79bd7ee00154231f
Ian Davis <ian@innovatian.com>
2012-07-28 16:04:54 -0700

    removing hash access until I can come up with a better closure method

--- examples/AddingFunctionsToPrototypes.ps1
+++ examples/AddingFunctionsToPrototypes.ps1
@@ -1,4 +1,4 @@
-function New-SapiVoice {
+﻿function New-SapiVoice {
   $prototype = new-prototype @{Message0 = "This is Message 0"}
   
   $prototype | new-function say {
@@ -7,19 +7,11 @@ function New-SapiVoice {
     ($speaker.Speak($message, 1)) | out-null
   }
   
-  $prototype.SayPreRecordedMessage = { . $prototype.say $Message0 }
-  
   # always return the base prototype
   $prototype
 }
 
 $voice = New-SapiVoice
+ 
 $voice.say("Hello, World!")
 # says 'Hello, World!'
-
-# note that because we added the SayPreRecordedMessage directly to the hash base, we have to . invoke it
-. $voice.SayPreRecordedMessage
-# says 'This is Message 0'
-
-$voice.SayPreRecordedMessage.Invoke()
-# says 'This is Message 0'
