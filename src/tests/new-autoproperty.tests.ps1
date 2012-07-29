f2d21245ae6039c6c0d1df5110cc5dc90338e4d3
Ian Davis <ian@innovatian.com>
2012-07-29 10:16:02 -0700

    Adding first tests

---
+++ src/tests/new-autoproperty.tests.ps1
@@ -0,0 +1,20 @@
+﻿$pwd = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".tests.", ".")
+. "$pwd\..\functions\$sut"
+
+function new-person {
+  param($radius = 3)
+  $prototype = (new-object psobject)
+  $prototype | new-autoproperty Name "John Doe"
+  $prototype | new-autoproperty Age
+  $prototype
+}
+
+Describe "Ensure-AutoPropertiesHaveTheirSuppliedOrDefaultValues" {
+    It "should use the supplied value for a property" {
+        (new-person).Name.should.be("John Doe")
+    }
+    It "should use null when no property value is supplied" {
+        (new-person) | ? { ($_.Age -ne $null) } | % { throw New-Object PesterFailure($null,$_.Age) }
+    }
+}
