dc28c904cc9e0bfe60aeaa02d36a1d61dfcf9156
Ian Davis <ian.f.davis@gmail.com>
2012-11-04 06:22:31 -0800

    Adding Examples project.

---
+++ src/Archetype.Examples/NotifyPropertyChangesExamples.cs
@@ -0,0 +1,173 @@
+﻿#region License
+
+// 
+// Copyright (c) 2012, Ian Davis
+// 
+// Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
+// See the file LICENSE.txt for details.
+// 
+
+#endregion
+
+#region Using Directives
+
+using System;
+using System.ComponentModel;
+using NUnit.Framework;
+
+#endregion
+
+namespace Archetype.Examples
+{
+    public interface INotifyPropertyChanges : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        void OnPropertyChanged( string propertyName = "" );
+        void OnPropertyChanging( string propertyName = "" );
+    }
+
+    public class NotifyPropertyChangesModule : INotifyPropertyChanges
+    {
+        #region INotifyPropertyChanges Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        public virtual void OnPropertyChanged( string propertyName = "" )
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if ( handler != null )
+            {
+                handler( this, new PropertyChangedEventArgs( propertyName ) );
+            }
+        }
+
+        public virtual void OnPropertyChanging( string propertyName = "" )
+        {
+            PropertyChangingEventHandler handler = PropertyChanging;
+            if ( handler != null )
+            {
+                handler( this, new PropertyChangingEventArgs( propertyName ) );
+            }
+        }
+
+        #endregion
+    }
+
+    public class Person : DelegatingObject
+    {
+        private int _Age;
+        private string _Name;
+
+        public Person()
+                : this( new NotifyPropertyChangesModule() )
+        {
+        }
+
+        public Person( params object[] modules )
+                : base( modules )
+        {
+        }
+
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                This.OnPropertyChanging( "Name" );
+                if ( _Name != value )
+                {
+                    _Name = value;
+                    This.OnPropertyChanged( "Name" );
+                }
+            }
+        }
+
+        public int Age
+        {
+            get { return _Age; }
+            set
+            {
+                Inpc.OnPropertyChanging( "Age" );
+                if ( _Age != value )
+                {
+                    _Age = value;
+                    Inpc.OnPropertyChanged( "Age" );
+                }
+            }
+        }
+
+        private dynamic This
+        {
+            get { return this; }
+        }
+
+        internal INotifyPropertyChanges Inpc
+        {
+            get { return This; }
+        }
+    }
+
+    [TestFixture]
+    public class PersonExamples
+    {
+        [Test]
+        public void SharedModulesToShareBehavior()
+        {
+            var module = new NotifyPropertyChangesModule();
+            module.PropertyChanged +=
+                    ( sender, args ) => Console.WriteLine( "The field {0} has changed.", args.PropertyName );
+            module.PropertyChanging +=
+                    ( sender, args ) => Console.WriteLine( "The field {0} is changing.", args.PropertyName );
+            var inigo = new Person( module ) { Name = "Inigo" };
+            var ian = new Person( module ) { Name = "Ian" };
+
+            inigo.Age = 45;
+            ian.Age = 30;
+        }
+
+        [Test]
+        public void UsingModelObjectAsDynamic()
+        {
+            dynamic person = new Person();
+            // The cast to the interface will work
+            INotifyPropertyChanges inpc = person;
+            inpc.PropertyChanged +=
+                    ( sender, args ) => Console.WriteLine( "The field {0} has changed.", args.PropertyName );
+            inpc.PropertyChanging +=
+                    ( sender, args ) => Console.WriteLine( "The field {0} is changing.", args.PropertyName );
+            // We have full IntelliSense when working with person.
+            // but now accessing .Name looses IntelliSense
+            person.Name = "Inigo Montoya";
+        }
+
+        [Test]
+        public void UsingModelObjectAsStronglyTyped()
+        {
+            var person = new Person();
+            // Casting first to dynamic triggers the DelegatingObjects casting system
+            INotifyPropertyChanges inpc = (dynamic) person;
+            inpc.PropertyChanged +=
+                    ( sender, args ) => Console.WriteLine( "The field {0} has changed.", args.PropertyName );
+            inpc.PropertyChanging +=
+                    ( sender, args ) => Console.WriteLine( "The field {0} is changing.", args.PropertyName );
+            // We have full IntelliSense when working with person.
+            // We have full IntelliSense when working with person.
+            person.Name = "Inigo Montoya";
+        }
+
+        [Test]
+        public void UsingModelWithProxyCastingProperty()
+        {
+            var person = new Person();
+            // The cast to the interface will work
+            person.Inpc.PropertyChanged +=
+                    ( sender, args ) => Console.WriteLine( "The field {0} has changed.", args.PropertyName );
+            person.Inpc.PropertyChanging +=
+                    ( sender, args ) => Console.WriteLine( "The field {0} is changing.", args.PropertyName );
+            // We have full IntelliSense when working with person.
+            // but now accessing .Name looses IntelliSense
+            person.Name = "Inigo Montoya";
+        }
+    }
+}
